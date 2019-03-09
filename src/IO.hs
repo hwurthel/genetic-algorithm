2bbce08239deb0ec0521c192da388a4a020ef150
wurthel <salmanov.vh@gmail.com>
2019-03-09 19:57:14 +0300

    Возможность вывода в файл  текущей популяции.

--- src/IO.hs
+++ src/IO.hs
@@ -1,6 +1,10 @@
 -- Модуль взаимодействия программы
 -- с внешними программами
-module IO where
+module IO 
+    (
+      computeLambda
+    , writeInFile
+    ) where
     
 import System.IO
 import System.Directory   (removeFile, doesFileExist)
@@ -8,26 +12,29 @@ import Control.Concurrent (threadDelay)
 
 import Protein
 
-outFileName = "output"
-inFileName  = "input"
-timeWait    = 5*10^6 -- микросекунды
+out_file = "oufile"
+tmp_of = "tmp_out"
+tmp_if  = "tmp_in"
+time_wait    = 5*10^6 -- микросекунды
 
 computeLambda :: [Protein] -> IO [Protein]
-computeLambda p = do
-    hOut <- openFile outFileName WriteMode
-    mapM_ (\p' -> hPutStrLn hOut $ protein p') p
-    hClose hOut
-    hIn <- wait True (openFile inFileName ReadMode)
-    res <- mapM (\p' -> hGetLine hIn >>= readIO >>= return . update p') p
-    hClose hIn
-    removeFile outFileName
-    removeFile inFileName
+computeLambda ps = do
+    withFile tmp_of WriteMode writeProtein
+    wait True 
+    res <- withFile tmp_if ReadMode readProtein
+    removeFile tmp_of
+    removeFile tmp_if
     return res
     where 
+        writeProtein = \hdl -> mapM_ (\p' -> hPutStrLn hdl $ protein p') ps
+        readProtein = \hdl -> mapM (\p' -> hGetLine hdl >>= return . (update p'). read) ps
         update p x = p { lambda = Just x }
-        wait False f = f
-        wait True  f = do
-            e <- doesFileExist inFileName 
-            if e then wait False f
-            else do threadDelay timeWait
-                    wait True f
+        wait False = return ()
+        wait True  = do 
+            e <- doesFileExist tmp_if 
+            if e then wait False
+            else threadDelay time_wait >> wait True
+
+writeInFile :: String -> [Protein] -> IO [Protein]
+writeInFile msg ps = withFile out_file AppendMode writeProtein >> return ps
+            where writeProtein = \hdl -> hPutStrLn hdl msg >> mapM_ (\p' -> hPutStrLn hdl $ show p') ps
