555677b59b3a9514058ee7d358e1e38884a3ba68
wurthel <salmanov.vh@gmail.com>
2019-03-09 21:32:49 +0300

    Реализован процесс эволюции.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,20 @@
 module Main where
 
+import StopCondition
 import Evolution
 import Protein
 import IO
 
 main :: IO ()
-main = print 1
-
--- Схема шага эволюции:
--- Кроссинговер -> Мутации -> Селекция
--- !!! Необходимо не забыть реализовать
--- !!! запись всех получаемых белков в процессе работы
--- Генерация популяции -> Кроссинговер -> Мутации -> Селекция
--- Селекции также предшествует операция ранжирования. 
+main = do
+    let stepOfEvolution = \x -> x >>= crossover >>= mutation >>= computeLambda >>= selection
+        process n (x, xs) = do
+            x'  <- stepOfEvolution x
+            xs' <- return $ xs <> x'
+            writeInFile ("Step " <> show n <> "\n") x'
+            if isStop n then return xs'
+            else process (n + 1) (return x', xs') 
+    res <- process 1 (generatePopulation, [])
+    writeInFile "--------------\n--------------\n" []
+    writeInFile "Result:\n" (sortPopulation res)
+    return ()
