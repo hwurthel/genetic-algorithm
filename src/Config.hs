e97e2672d05acc640d34f6c7cb1f52d88f9e7ec7
wurthel <salmanov.vh@gmail.com>
2019-03-18 23:58:09 +0300

    Добавлена возможность указывать наборы заменяемых аминокислот в config-файле

--- src/Config.hs
+++ src/Config.hs
@@ -1,13 +1,17 @@
 module Config where
 
 import System.IO.Unsafe
+import Data.List.Split
+import Data.List
 
 type Config = [(String, String)]
 
 config_file = "config"
 
 readConfig :: Config
-readConfig = [(key, val) | line <- filter (`notElem` [""]) $ lines s, let (key : _ : val : _) = words line]
+readConfig = [(key, val) | 
+                line <- filter (\x -> (not $ "--" `isPrefixOf` x) && x `notElem` [""]) $ lines s,
+                let (key : _ : val : _) = words line]
     where s = unsafePerformIO $ readFile config_file 
 
 get :: String -> (String -> a) -> a
@@ -15,21 +19,55 @@ get key f = case lookup key readConfig of
     Nothing -> error ("get: not found: " ++ key)
     Just x  -> f x
 
-out_file, tmp_of, tmp_if :: String
-out_file  = get "out_file" id                   -- Имя выходного файла
-tmp_of    = get "tmp_of" id                     -- Имя выходной файла для обсчета параметров белка
-tmp_if    = get "tmp_if" id                     -- Имя входного файла, где находятся обсчитанные параметры
+-- | Шаблонный белок
+tmp_protein :: String
+tmp_protein = get "tmp_protein" id
 
+-- | Параметр шаблонного белка
+tmp_lambda :: Maybe Double
+tmp_lambda = get "tmp_lambda" read
+
+-- | Набор заменяемых аминокислот
+bros_list :: [([Char], Int)]
+bros_list = get "bros" readBros
+    where readBros s = [(id x, read y) | p <- splitOn "_" s, let (x:y:[]) = splitOn ":" p]
+
+-- | Выходной файл
+out_file :: String
+out_file = get "out_file" id 
+
+-- | Входной файл для обсчета параметров белка
+tmp_of :: String
+tmp_of = get "tmp_of" id 
+
+-- | Входной файл, где находятся обсчитанные параметры
+tmp_if :: String
+tmp_if = get "tmp_if" id
+
+-- | Время задержки перед проверка файла @tmp_if@, в мкс
 time_wait :: Int
-time_wait = get "time_wait" read                -- Время задержки перед проверка файла @tmp_if@
+time_wait = get "time_wait" read
 
-eval_param                :: Double  
-pop_size                  :: Int
-prob_cros, prob_cros_gene :: Double 
-prob_mut, prob_mut_gene   :: Double 
-eval_param      = get "eval_param" read         -- Параметр для функции оценки
-pop_size        = get "pop_size" read           -- Размер популяции
-prob_cros       = get "prob_cros" read          -- Вероятность того, что хромосома будет участвовать в кроссинговере
-prob_cros_gene  = get "prob_cros_gene" read     -- Вероятность того, что ген в хромосоме подвергнется кроссинговеру
-prob_mut        = get "prob_mut" read           -- Вероятность того, что хромосома будет участвовать в мутации
-prob_mut_gene   = get "prob_mut_gene" read      -- Вероятность того, что ген в хромосоме подвергнется мутации
+-- | Параметр для функции оценки
+eval_param :: Double
+eval_param = get "eval_param" read
+
+-- | Размер популяции
+pop_size :: Int
+pop_size = get "pop_size" read
+
+-- | Вероятность того, что хромосома будет участвовать в кроссинговере
+prob_cros :: Double 
+prob_cros = get "prob_cros" read
+
+-- | Вероятность того, что ген в хромосоме подвергнется кроссинговеру
+prob_cros_gene :: Double
+prob_cros_gene = get "prob_cros_gene" read
+
+-- | Вероятность того, что хромосома будет участвовать в мутации
+prob_mut :: Double
+prob_mut = get "prob_mut" read
+
+-- | Вероятность того, что ген в хромосоме подвергнется мутации
+prob_mut_gene :: Double
+prob_mut_gene = get "prob_mut_gene" read
